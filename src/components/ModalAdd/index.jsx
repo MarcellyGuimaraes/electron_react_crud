526ef4b9ce3c67ec28efe6b00c27395ff359d857
MarcellyGuimaraes <souzaguimaraesmarcelly@gmail.com>
2023-02-14 14:19:30 -0300

    feat: adicionando 'limpeza' do formulário add e edit quando um usuário é criado//editado

--- src/components/ModalAdd/index.jsx
+++ src/components/ModalAdd/index.jsx
@@ -65,6 +65,8 @@ const ModalAdd = ({ show, onClose, get, users }) => {
                   type="button"
                   onClick={() => {
                     addUser(get, nome, cpf, users)
+                    setNome('')
+                    setCpf('')
                     onClose()
                   }}
                   className="w-full text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
--- src/components/ModalEdit/index.jsx
+++ src/components/ModalEdit/index.jsx
@@ -73,6 +73,8 @@ const ModalEdit = ({
                   type="button"
                   onClick={() => {
                     editUser(id, get, nome, cpf, users)
+                    setNome('')
+                    setCpf('')
                     onClose()
                   }}
                   className="w-full text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
